a538669fc46e63e34de73c6bb337d3a3f91890ff
Bianca Golin Flores <biancagolin@hotmail.com>
2021-09-19 23:27:57 -0300

    Segunda atividade realizada para a disciplina Dispositivos Móveis

--- App.js
+++ App.js
@@ -2,6 +2,30 @@ import * as React from 'react';
 import { Text, View, StyleSheet, FlatList, Pressable, Image,Modal } from 'react-native';
 import Constants from 'expo-constants';
 
+async function executeGet(url,jsonState){
+    //get síncrono com o uso do fetch
+    await fetch(url)
+    .then(response => {
+          if (response.status === 200) {
+            console.log('sucesso');
+            response.json().then(function(result){ 
+
+              //console.log(result);
+              jsonState(result)
+
+              });
+          } else {
+            throw new Error('Erro ao consumir a API!');
+          }
+      })
+      .then(response => {
+        //console.debug(response);
+      }).catch(error => {
+        console.error(error);
+      });
+  }
+
+
 
 const ShowDetalhes = ({display,toogleModal,mensagem}) => (   
     <Modal
@@ -23,7 +47,7 @@ const ShowDetalhes = ({display,toogleModal,mensagem}) => (
         
  )
 
-const Pessoa = ({nome,comentario,foto}) => {
+const Pessoa = ({nome,email,link}) => {
     
     //state para controle do Modal
     const [modal,setModal] = React.useState(false)
@@ -34,13 +58,13 @@ const Pessoa = ({nome,comentario,foto}) => {
 
     return(
     <View>
-      <ShowDetalhes display={modal} toogleModal={mudaModal} mensagem={comentario}/>
+      <ShowDetalhes display={modal} toogleModal={mudaModal} mensagem={email}/>
       
       <Pressable onPress={mudaModal}>
         <Image
           style={styles.tinyLogo}
           source={{
-            uri: foto,
+            uri: link,
           }}
         />
 
@@ -51,40 +75,6 @@ const Pessoa = ({nome,comentario,foto}) => {
 }
 
 
-const DATA = [
-        {
-          "id": 1,
-          "name": "Morty",
-          "body": "Criado por Justin Roiland e Dan Harmon, Morty é um ansioso garoto de 14 anos baseado em Marty McFly de Back to the Future.",
-          "foto": "https://pbs.twimg.com/profile_images/482902136427397120/f6h3mYEu.jpeg"
-        },
-        {
-          "id": 2,
-          "name": "Ricky",
-          "body": "Richard D. Rick Sanchez III é um dos dois protagonistas homônimos da série animada de televisão de Adult Swim, Rick e Morty. Criado por Justin Roiland e Dan Harmon e dublado pelo primeiro, Sanchez é um cientista alcoólatra misantrópico",
-          "foto": "https://imagensemoldes.com.br/wp-content/uploads/2021/04/Ilustracao-Rick-and-Morty-PNG.png"
-        },
-        {
-          "id": 3,
-          "name": "Beth",
-          "body": "Beth Smith é uma das personagens principais da série de animação americana Rick e Morty. Criada por Justin Roiland e Dan Harmon, Beth é uma veterinária especializada em cirurgia de cavalos",
-          "foto": "https://rickandmortyapi.com/api/character/avatar/4.jpeg"
-        },
-        {
-          "id": 4,
-          "name": "Jerry",
-          "body": "Jerry Smith é um dos personagens principais de Rick e Morty. Jerry é marido de Beth Smith, pai de Summer Smith e Morty Smith, e genro de Rick Sanchez.",
-          "foto": "https://mystickermania.com/cdn/stickers/rick-and-morty/jerry-smith-sock-toy-512x512.png"
-        },
-        {
-          "id": 5,
-          "name": "Summer",
-          "body": "Summer Smith é um dos personagens principais da série de animação americana Rick e Morty. Criado por Justin Roiland e Dan Harmon, Summer é uma adolescente convencional e muitas vezes superficial e inicialmente de 17 anos, que está obcecada em melhorar seu status social entre seus pares.",
-          "foto": "https://i.pinimg.com/originals/93/3f/ce/933fce7bf2db6fed7b2a38569fd5f7c4.png"
-        }
-    ];
-
-
 
 //item com uma arrow function
 /*const meuItemObj = ({item}) => (
@@ -93,32 +83,38 @@ const DATA = [
     </View>
 )*/
 
-
-
 export default function App() {
+
+  const [jsonData,setJsonData] = React.useState({})
+
+  executeGet("https://testimonialapi.toolcarton.com/api",setJsonData)
 
   //função que renderiza cada item do FlatList
   function meuItem({item}){
-       
+    //let nomeCompleto = item.first_name + " " + item.last_name
+    
     return(
-      <Pessoa nome={item.name}
-              comentario={item.body}
-              foto={item.foto}
+      <Pessoa nome={item.name} 
+              link={item.avatar}
+              email={item.message}
       />
     )
   }
   
 
+
   return (
 
     <View style={styles.container}>
 
+    <Text style={styles.top}>review</Text>  
+
       <FlatList
-        data={DATA}
+        data={jsonData}
         renderItem={meuItem}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.id} 
       />
-      
+
     </View>
   );
 }
@@ -129,7 +125,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
-    padding: 2,
+    padding: 8,
+  },
+  top: {
+    textAlign:"center",
+    fontWeight:"bold",
+    fontSize:25,
+    flex: 0.5,
+    backgroundColor: "gold",
+    borderWidth: 0,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
   },
   paragraph: {
     margin: 12,
@@ -137,7 +143,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
-    backgroundColor: 'yellow'
+    backgroundColor: 'gray'
   },
   tinyLogo: {
     width: 50,
